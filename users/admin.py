35b45b3c46ffe61d197eb4bd03a14cac46736d28
Sam Dindyal <sam.dindyal@me.com>
2021-05-13 15:48:43 -0400

    Adds user to admin page

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from django.contrib.auth import get_user_model                                                   
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+CustomUser = get_user_model()
+
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    list_display = ['email', 'username', ] 
+
+admin.site.register(CustomUser, CustomUserAdmin) 
